b2d2270e6f01788a0edbc1158653906c36e6077b
zeys0 <muhammadrafli09.ml@gmail.com>
2024-12-07 12:17:17 +0700

    fetch data restoran dari api google maps

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ from folium.plugins import MarkerCluster
 from flask_migrate import Migrate
 from models import db, Place
 import json
+import googlemaps
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///food_map.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["GOOGLE_API_KEY"] = (
+    "AIzaSyCU4mFWGN9UjhKUXYoEfcQR-TLmHfPPN98"  # MASUKKAN API KEY MILIK KAMU
+)
 app.secret_key = "secret_key"
 db.init_app(app)
 migrate = Migrate(app, db)
@@ -35,8 +39,7 @@ preview_map.save("static/preview/preview_map.html")
 
 
 @app.route("/")
-def hello():
-
+def home():
     return render_template("main/home.html", enable_scroll_nav=True)
 
 
@@ -78,6 +81,54 @@ def map():
         )
     preview_map.save("static/preview/preview_map.html")
     return render_template("main/map.html", enable_scroll_nav=False)
+
+
+# Mengambil data restoran dari API GOOGLE MAPS
+# Google Maps Client
+gmaps = googlemaps.Client(key=app.config["GOOGLE_API_KEY"])
+
+
+@app.route("/fetch_google_maps", methods=["GET"])
+def fetch_google_maps():
+    # Koordinat dan radius pencarian
+    semarang_location = {"lat": -7.005145, "lng": 110.438125}
+
+    radius = 60000  # mengambil data restoran dalam satuan meter dari koordinat
+
+    places_result = gmaps.places_nearby(
+        location=semarang_location,
+        radius=radius,
+        type="restaurant",
+    )
+
+    for place in places_result.get("results", []):
+        name = place.get("name")
+        lat = place["geometry"]["location"]["lat"]
+        lon = place["geometry"]["location"]["lng"]
+        img_url = None
+        if place.get("photos"):
+            photo_reference = place["photos"][0]["photo_reference"]
+            img_url = (
+                f"https://maps.googleapis.com/maps/api/place/photo"
+                f"?maxwidth=400&photoreference={photo_reference}&key={app.config['GOOGLE_API_KEY']}"
+            )
+        price_range = place.get("price_level", "Tidak diketahui")
+        desc = ", ".join(place.get("types", []))
+
+        # Simpan ke database
+        new_place = Place(
+            name=name,
+            lat=lat,
+            lon=lon,
+            image_url=img_url,
+            price_range=price_range,
+            description=desc,
+        )
+        db.session.add(new_place)
+
+    db.session.commit()
+    flash("Data restoran dari Google Maps berhasil ditambahkan ke database.")
+    return redirect(url_for("map"))
 
 
 if __name__ == "__main__":
